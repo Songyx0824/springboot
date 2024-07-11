7d42300fc8c2a1807552428fb6e754f26eb24ce2
Songyx0824 <18045025768@163.com>
2024-07-11 22:32:59 +0800

    spring-boot-starter

--- chapter01-stream/src/main/java/com/song/atguigu/lambda/FunctionDemo.java
+++ chapter01-stream/src/main/java/com/song/atguigu/lambda/FunctionDemo.java
@@ -35,4 +35,41 @@ public class FunctionDemo {
         }
 
     }
+
+
+    @Test
+    public void FunctionDemoTest2(){
+        Supplier<String> supplier = () -> "41";
+
+        //断言 Predicate 验证是否是一个数字
+        Predicate<String> isNumber = str -> str.matches("-?\\d+(\\.\\d+)?");
+
+        //Function<String , Integer> change = str -> Integer.parseInt(str);
+        Function<String , Integer> change = Integer::parseInt;
+
+
+        Consumer<Integer> consumer = integer -> {
+            if(integer%2 == 0) {
+                System.out.println("偶数: " + integer);
+            }else {
+                System.out.println("基数: " + integer);
+            }
+        };
+
+        mymethod(str -> str.matches("-?\\d+(\\.\\d+)?"),
+                ()->"777",
+                System.out::println,
+                Integer::parseInt
+                );
+
+    }
+
+    private static void mymethod(Predicate<String> isNumber, Supplier<String> supplier, Consumer<Integer> consumer, Function<String, Integer> change) {
+        if(isNumber.test(supplier.get())){
+            consumer.accept(change.apply(supplier.get()));
+        }else {
+            System.out.println("非法的数字");
+        }
+    }
+
 }
